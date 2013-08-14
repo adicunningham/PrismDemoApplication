38068a5887ea9e045d612a28e60549d42ff6e26d
Adrian Cunningham <adrian.cunningham@gmail.com>
2013-08-14 00:50:39 -0300

    Initial Project structure

---
+++ Src/Modules/ModuleA/ModuleAModule.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Practices.Prism.Modularity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Practices.Prism.Regions;
+using Microsoft.Practices.Unity;
+using ModuleA.Properties;
+using PrismDemoApplication.Infrastructure;
+
+namespace ModuleA
+{
+    public class ModuleAModule : IModule
+    {
+        private IUnityContainer _container;
+        private IRegionManager _regionManager;
+
+        public ModuleAModule(IUnityContainer container, IRegionManager regionManager)
+        {
+            _container = container;
+            _regionManager = regionManager;
+        }
+
+        public void Initialize()
+        {
+
+
+            IRegion region = _regionManager.Regions[RegionNames.ToolbarRegion];
+
+            region.Add(_container.Resolve<RibbonView>());
+            //region.Add(_container.Resolve<ToolBarView>());
+            //region.Add(_container.Resolve<ToolBarView>());
+            //region.Add(_container.Resolve<ToolBarView>());
+
+            _regionManager.RegisterViewWithRegion(RegionNames.ContentRegion, typeof (ContentView));
+
+            
+        }
+    }
+}
---
+++ Src/PrismDemoApplication.Infrastructure/RegionNames.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrismDemoApplication.Infrastructure
+{
+    public class RegionNames
+    {
+        public static string ToolbarRegion = "ToolbarRegin";
+        public static string ContentRegion = "ContentRegion";
+    }
+}
