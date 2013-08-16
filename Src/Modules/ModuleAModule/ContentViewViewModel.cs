0d24dc29c160c6215057c83c0f31edbaf6820237
Adrian Cunningham <adrian.cunningham@gmail.com>
2013-08-16 01:32:22 -0300

    Implemented IViewModel First approach.

---
+++ Src/Modules/ModuleAModule/ContentViewViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModuleA
+{
+    public class ContentViewViewModel : IContentViewViewModel
+    {
+        public PrimsDemoApplication.Infrastructure.IView View {get; set; }
+
+        public ContentViewViewModel(IContentView view)
+        {
+            View = view;
+            View.ViewModel = this;
+        }
+    
+    }
+}
